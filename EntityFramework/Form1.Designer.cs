5f6109ae2416e5d987a646cbde5cad36e9c94345
u36286 <u36286@UMC-C817>
2020-11-27 16:43:56 +0700

    Add project files.

---
+++ EntityFramework/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace EntityFramework
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.dtgvdata = new System.Windows.Forms.DataGridView();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtaddress = new System.Windows.Forms.TextBox();
+            this.txtscore = new System.Windows.Forms.TextBox();
+            this.txtmalop = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtmasv = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvdata)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(51, 337);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 25);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Thêm";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(155, 337);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 25);
+            this.btnEdit.TabIndex = 1;
+            this.btnEdit.Text = "Sửa";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(279, 337);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(75, 25);
+            this.btnDel.TabIndex = 2;
+            this.btnDel.Text = "Xóa";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(570, 337);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(75, 25);
+            this.btnShow.TabIndex = 3;
+            this.btnShow.Text = "Xem";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // dtgvdata
+            // 
+            this.dtgvdata.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgvdata.Location = new System.Drawing.Point(12, 159);
+            this.dtgvdata.Name = "dtgvdata";
+            this.dtgvdata.Size = new System.Drawing.Size(650, 150);
+            this.dtgvdata.TabIndex = 4;
+            this.dtgvdata.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgvdata_CellClick);
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(72, 12);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(100, 20);
+            this.txtname.TabIndex = 5;
+            // 
+            // txtaddress
+            // 
+            this.txtaddress.Location = new System.Drawing.Point(72, 76);
+            this.txtaddress.Name = "txtaddress";
+            this.txtaddress.Size = new System.Drawing.Size(100, 20);
+            this.txtaddress.TabIndex = 6;
+            // 
+            // txtscore
+            // 
+            this.txtscore.Location = new System.Drawing.Point(279, 12);
+            this.txtscore.Name = "txtscore";
+            this.txtscore.Size = new System.Drawing.Size(100, 20);
+            this.txtscore.TabIndex = 7;
+            // 
+            // txtmalop
+            // 
+            this.txtmalop.Location = new System.Drawing.Point(279, 80);
+            this.txtmalop.Name = "txtmalop";
+            this.txtmalop.Size = new System.Drawing.Size(100, 20);
+            this.txtmalop.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Tên";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Địa chỉ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(212, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Điểm";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(227, 83);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Mã lớp";
+            // 
+            // txtmasv
+            // 
+            this.txtmasv.Location = new System.Drawing.Point(528, 79);
+            this.txtmasv.Name = "txtmasv";
+            this.txtmasv.Size = new System.Drawing.Size(100, 20);
+            this.txtmasv.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 385);
+            this.Controls.Add(this.txtmasv);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtmalop);
+            this.Controls.Add(this.txtscore);
+            this.Controls.Add(this.txtaddress);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.dtgvdata);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvdata)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.DataGridView dtgvdata;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtaddress;
+        private System.Windows.Forms.TextBox txtscore;
+        private System.Windows.Forms.TextBox txtmalop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtmasv;
+    }
+}
+
---
+++ EntityFramework/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntityFramework
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+        Student_DBEntities db = new Student_DBEntities();
+        int selectRow;
+        void LoadData()
+        {
+
+
+            //var result =db.Students;
+            var result = from s in db.Students select new { s.Id, s.Name, s.Address, s.Score, s.Id_class };
+            dtgvdata.DataSource = result.ToList();
+
+
+        }
+        void AddSV()
+        {
+
+        }
+        void EditSV() { }
+        void DelSV() { 
+}
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Student stu = new Student();
+             //   stu.Id = int.Parse(txtMa.Text);
+                stu.Name = txtname.Text;
+                stu.Address = txtaddress.Text;
+                stu.Score = int.Parse(txtscore.Text);
+                stu.Id_class = int.Parse(txtmalop.Text);
+                db.Students.Add(stu);
+                db.SaveChanges();
+                LoadData();
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.ToString());
+            }
+
+
+        }
+      
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            DataGridViewRow datagv = dtgvdata.Rows[selectRow];
+            datagv.Cells[1].Value =txtname.Text;
+            datagv.Cells[2].Value =txtaddress.Text;
+            datagv.Cells[3].Value =txtscore.Text;
+            datagv.Cells[4].Value =txtmalop.Text;
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            selectRow = dtgvdata.CurrentCell.RowIndex;
+            dtgvdata.Rows.RemoveAt(selectRow);
+
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void dtgvdata_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void dtgvdata_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            selectRow = e.RowIndex;
+
+            DataGridViewRow row = dtgvdata.Rows[selectRow];
+            txtname.Text = row.Cells[1].Value.ToString();
+            txtaddress.Text = row.Cells[2].Value.ToString();
+            txtscore.Text = row.Cells[3].Value.ToString();
+            txtmalop.Text = row.Cells[4].Value.ToString();
+
+        }
+    }
+}
